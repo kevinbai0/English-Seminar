7fc3e8b2569cf7d8ca9225f274f4f1292ac9fe0f
kevinbai0 <kevin.c.bai0@gmail.com>
2018-11-16 14:58:02 -0500

    added animations to go out in presentation order over 30 seconds

--- src/components/CommentatorSplashPage.js
+++ src/components/CommentatorSplashPage.js
@@ -1,7 +1,9 @@
 import React from "react";
+import anime from "animejs";
 class CommentatorSplashPage extends React.Component {
     constructor(props) {
         super(props);
+        this.splashPageRef = React.createRef();
         this.comicReliefRef = React.createRef();
         this.humorRef = React.createRef();
         this.alwaysSayRef = React.createRef();
@@ -15,7 +17,75 @@ class CommentatorSplashPage extends React.Component {
 
     componentDidUpdate() {
         if (this.props.state === "entered") {
-            this.comicReliefRef.current.style.transform = "translate(-500px, 0)";
+            let timeline = anime.timeline();
+            timeline.add({
+                targets: this.alwaysSayRef.current,
+                duration: 4500,
+                scale: [0.1, 1],
+                translateX: [-window.innerWidth / 2, 0],
+                opacity: [0, 1],
+                easing: "easeInOutQuad",
+                offset: 500,
+                duration: 3000
+            }).add({
+                targets: this.comicReliefRef.current,
+                duration: 5000,
+                translateX: [-500, 0],
+                rotate: [90, 90],
+                easing: "easeInOutQuad",
+                offset: "-=1000",
+            }).add({
+                targets: this.humorRef.current,
+                translateX: [window.innerWidth, 0],
+                rotateX: [360, 0],
+                duration: 3500,
+                easing: "easeInOutQuad",
+                offset: "-=1000",
+            }).add({
+                targets: this.commentaryRef.current,
+                translateX: [-2 * window.innerWidth, 0],
+                duration: 7000,
+                offset: "-=1500",
+                easing: "easeInQuad",
+            }).add({
+                targets: this.contradictsRef.current,
+                translateX: [1000, 0],
+                duration: 5000,
+                offset: "-=500",
+                easing: "easeOutQuad"
+            }).add({
+                targets: this.noAuthorityRef.current,
+                translateY: [-500, 0],
+                rotate: [-90,-90],
+                duration: 5000,
+                easing: "easeOutQuad",
+                offset: "-=500",
+            }).add({
+                targets: this.unheardRef.current,
+                translateX: [500, 0],
+                scale: [0.5, 1],
+                easing: "easeInCubic",
+                duration: 3000,
+                offset: "-=500"
+            }).add({
+                targets: this.voicesOpinionRef.current,
+                translateY: [500, 0],
+                opacity: [0,1],
+                easing: "easeInOutQuad",
+                duration: 5000,
+                offset: "-=500"
+            }).add({
+                targets: this.peanutRef.current,
+                translateX: [-500, 0],
+                translateY: [150, 0],
+                duration: 6000,
+                offset: "-=2000",
+                easing: "easeInOutQuad"
+            })
+
+            timeline.finished.then(() => {
+                this.animationsFinished();
+            })
         }
     }
     render = () => {
@@ -30,7 +100,7 @@ class CommentatorSplashPage extends React.Component {
             classNameExtension=" entering entered";
             // reposition elements
         }
-        return <div className="commentator-splash-page">
+        return <div className="commentator-splash-page" ref={this.splashPageRef}>
             <div className={"comic-relief" + classNameExtension} ref={this.comicReliefRef}>Comic Relief</div>
             <div className={"humor" + classNameExtension} ref={this.humorRef}>Humor</div>
             <div className={"always-something-say" + classNameExtension} ref={this.alwaysSayRef}>Always has something to say</div>
@@ -41,6 +111,10 @@ class CommentatorSplashPage extends React.Component {
             <div className={"no-authority" + classNameExtension} ref={this.noAuthorityRef}>No Authority</div>
             <div className={"often-unheard" + classNameExtension} ref={this.unheardRef}>Often go unheard</div>
         </div>
+    }
+
+    animationsFinished() {
+        this.props.animationsFinished();
     }
 }
 
