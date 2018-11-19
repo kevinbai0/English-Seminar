02f5301aff3e8ee033036494d9cb9472645a75be
kevinbai0 <kevin.c.bai0@gmail.com>
2018-11-19 01:49:51 -0500

    Updates

---
+++ precache-manifest.52fe0e24a0a82ab75040062825afbe50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "79808abba4cbcee7a837",
+    "url": "/English-Seminar/static/js/runtime~main.79808abb.js"
+  },
+  {
+    "revision": "9d1d2f50991be67d7a86",
+    "url": "/English-Seminar/static/js/main.9d1d2f50.chunk.js"
+  },
+  {
+    "revision": "e601f21a84701729a2ef",
+    "url": "/English-Seminar/static/js/1.e601f21a.chunk.js"
+  },
+  {
+    "revision": "9d1d2f50991be67d7a86",
+    "url": "/English-Seminar/static/css/main.d21d8d99.chunk.css"
+  },
+  {
+    "revision": "6e97dec806adf49ec839974d1a081ec2",
+    "url": "/English-Seminar/index.html"
+  }
+];
